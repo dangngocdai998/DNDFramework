ceeb6f69ec376f953583afc3744ef7f8f3cd882d
Dai_HomePC <dangngocdai998@gmail.com>
2024-12-06 17:51:12 +0700

    Set EndTransition on Loadscene

--- DNDFramework/Common/LoadSceneManager.cs
+++ DNDFramework/Common/LoadSceneManager.cs
@@ -7,57 +7,50 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneManager : SingletonMonoBehaviour<LoadSceneManager>
 {
-    public const string sceneMenu = "Menu";
-    public const string sceneSpl = "Spl";
-    public const string sceneGamePlay = "GamePlay";
-
-
-
-
     #region LoadSceneAsync
-    private IEnumerator WaitLoadAsyncScene(string name, Action completed, bool closeFade)
-    {
-        Debug.Log("Scene " + name + " is loading...");
-        /* if (String.Compare(name, houseScene, false) == 0 && String.Compare(houseScene, SceneManager.GetActiveScene().name, false) == 0)
-        {
-            TransitionFX.Instance.FakeRunProgress();
-            completed?.Invoke();
+    // private IEnumerator WaitLoadAsyncScene(string name, Action completed, bool closeFade)
+    // {
+    //     Debug.Log("Scene " + name + " is loading...");
+    //     /* if (String.Compare(name, houseScene, false) == 0 && String.Compare(houseScene, SceneManager.GetActiveScene().name, false) == 0)
+    //     {
+    //         TransitionFX.Instance.FakeRunProgress();
+    //         completed?.Invoke();
 
-        }
-        else
-        { */
-        // CanvasManager.ClearAllUI();
-        PoolingManager.Instance.DisableAllObject();
-        EventDispatcher.ClearAllListener();
-        CustomInput.ResetInput();
+    //     }
+    //     else
+    //     { */
+    //     // CanvasManager.ClearAllUI();
+    //     PoolingManager.Instance.DisableAllObject();
+    //     EventDispatcher.ClearAllListener();
+    //     CustomInput.ResetInput();
 
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
-        while (!asyncLoad.isDone)
-        {
-            TransitionFX.Instance.ChangeValueProgress(asyncLoad.progress);
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         TransitionFX.Instance.ChangeValueProgress(asyncLoad.progress);
 
-            yield return null;
-        };
-        completed?.Invoke();
-        TransitionFX.Instance.ChangeValueProgress(1);
-        // }
+    //         yield return null;
+    //     };
+    //     completed?.Invoke();
+    //     TransitionFX.Instance.ChangeValueProgress(1);
+    //     // }
 
-        /*  if (closeFade && TransitionFX.Exists())
-         {
-             yield return new WaitForSecondsRealtime(2f);
-             TransitionFX.I.EndFade();
-         } */
+    //     /*  if (closeFade && TransitionFX.Exists())
+    //      {
+    //          yield return new WaitForSecondsRealtime(2f);
+    //          TransitionFX.I.EndFade();
+    //      } */
 
-        Debug.Log("Scene " + name + " is loaded!!!");
-    }
+    //     Debug.Log("Scene " + name + " is loaded!!!");
+    // }
     #endregion
 
-    public void LoadScene(string name, Action completed)
+    public void LoadScene(string name, Action completed, bool isEndTransition = true)
     {
-        TransitionFX.Instance.StartFade(() => { StartCoroutine(WaitLoadAsyncScene(name, completed)); });
+        TransitionFX.Instance.StartFade(() => { StartCoroutine(WaitLoadAsyncScene(name, completed, isEndTransition)); });
     }
 
-    private IEnumerator WaitLoadAsyncScene(string name, Action completed)
+    private IEnumerator WaitLoadAsyncScene(string name, Action completed, bool isEndTransition)
     {
         Debug.Log("Scene " + name + " is loading...");
         CanvasManager.ClearAllUI();
@@ -73,12 +66,12 @@ public class LoadSceneManager : SingletonMonoBehaviour<LoadSceneManager>
             yield return null;
         };
         completed?.Invoke();
-
-        if (TransitionFX.Exists())
-        {
-            yield return new WaitForSecondsRealtime(0.5f);
-            TransitionFX.Instance.EndFade();
-        }
+        if (isEndTransition)
+            if (TransitionFX.Exists())
+            {
+                yield return new WaitForSecondsRealtime(0.5f);
+                TransitionFX.Instance.EndFade();
+            }
 
         Debug.Log("Scene " + name + " is loaded!!!");
     }
